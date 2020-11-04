3a57faf1645f1c75365508cdb802b5f60d72bfff
michlits <micahelduthee@gmail.com>
2020-11-04 16:19:15 +0200

    Changes in assigment3_2

--- Lab_3/src/assignment3_2.cpp
+++ Lab_3/src/assignment3_2.cpp
@@ -9,18 +9,18 @@ typedef ac_int<4,false> Card;
 class BlackJack{
 private :
     //internal state
-    ac_int<4,false> cnt, count_card;
+    ac_int<4,false> cnt;
     ac_int<5,false> res;
     Card card;
 
 public :
     // constructor-init internal state
-    BlackJack() {cnt = 0; count_card = 0; res = 0; }
+    BlackJack() {cnt = 0; res = 0; }
 
 
     void run(ac_channel<Card> &in_card, bool &end_round, bool &win){
 
-        if(in_card.available(1) && end_round == 0){
+        if(in_card.available(1)){
             card = in_card.read();
             cnt++;
             if(card > 1){
@@ -29,34 +29,20 @@ public :
                 res += 11;
             }
 
-            if(res > 21){
-                //std::cout<<"res > 21"<<std::endl;
-                win = 0;
-                end_round = 1;
-            } else if (res == 21){
-                //std::cout<<"res == 21"<<std::endl;
+            if((res < 21 && cnt ==5) || res == 21 || (res == 22 && cnt ==2)){
+                std::cout<<"You won"<<std::endl;
                 win = 1;
                 end_round = 1;
-            } else if (res == 22 && cnt == 2){
-                //std::cout<<"res == 22 && cnt == 2"<<std::endl;
-                win = 1 ;
-                end_round = 1;
-            }else if(res < 21 && cnt == 5){
-                //std::cout<<"res < 21 && cnt == 5"<<std::endl;
-                win = 1;
-                end_round = 1;
-            }else {
-                //std::cout<<"else"<<std::endl;
+            }else if (res > 21 || cnt == 5){
+                std::cout<<"You lost"<<std::endl;
                 win = 0;
+                end_round = 1;
+            } else{
                 end_round = 0;
             }
-
-            if(cnt == 5){
-                end_round = 1;
-            }
-            std::cout<<"res "<<res<<std::endl;
-            std::cout<<"win "<<win<<std::endl;
-            std::cout<<"end_round "<<end_round<<std::endl;
+            std::cout<<"Result "<<res<<std::endl;
+            //std::cout<<"win "<<win<<std::endl;
+            //std::cout<<"end_round "<<end_round<<std::endl;
         }
 
     }
@@ -65,7 +51,7 @@ public :
 
 int main(){
     int v1;
-    bool end_round = 0, win = 0;
+    bool end_round = 0, win;
     ac_channel<Card> in_card;
 
     BlackJack game;
@@ -73,15 +59,19 @@ int main(){
 
     srand(time(NULL));
     
-    for (int i = 0; i < 5; i++){
-        v1 = rand()% 9 + 1;
-        std::cout<<"card "<<v1<<std::endl;
-        in_card.write(v1);
-    }
 
-    for (int i = 0; i < 5; i++){
+    std::cout<<"Playing the game"<<std::endl;
+    while(end_round == 0 ){
+        v1 = rand()% 9 + 1;
+        std::cout<<"Card Value "<<v1<<std::endl;
+        in_card.write(v1);
         game.run(in_card, end_round, win);
     }
+    std::cout<<"win "<<win<<std::endl;
 
     return 0;
 }
+
+
+
+
