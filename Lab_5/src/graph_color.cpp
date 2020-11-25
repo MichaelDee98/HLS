24724f78b700cfe7895c83cc5343d9a7cb248543
michlits <micahelduthee@gmail.com>
2020-11-25 18:10:18 +0200

    Commiting graph_color

--- Lab_5/src/graph_color.cpp
+++ Lab_5/src/graph_color.cpp
@@ -7,24 +7,21 @@
 
 
 
-void coloring(ac_int<6,false> A[6]){
+void coloring(ac_int<6,false> A[6],ac_int<3,false> color_table[6],ac_int<3,false> &color_used){
     ac_int<6,false> temp ;
-    int color_table[6] = {1, 1, 1, 1, 1, 1};
+    
     for(int i = 0; i < 6; i++){
         temp=A[i];
-        std::cout<<"row no. "<<i<<std::endl;
+        //std::cout<<"row no. "<<i<<std::endl;
         for(int j = 0; j < 6; j++){
-             std::cout<<" "<<temp[j]<<" ";
+             //std::cout<<" "<<temp[j]<<" ";
             if(temp[j]==1 && color_table[i]==color_table[j]){
                 color_table[j]=color_table[j]+1;
             }
+            color_used =(color_table[j]>color_used)?color_table[j]:color_used;
         }
-        std::cout<<std::endl;
+        //std::cout<<std::endl;
     } 
-    for(int i=0;i<6;i++){
-        std::cout<<color_table[i];
-    }
-    std::cout<<std::endl;
 
 }
 
@@ -36,10 +33,10 @@ void coloring(ac_int<6,false> A[6]){
 
 int main(){
     srand(time(NULL));
-    
-    bool A[6][6];
+    ac_int<3,false> color_used=0 ;
+    ac_int<3,false> color_table[6] = {1, 1, 1, 1, 1, 1};
     ac_int<6,false> Adj_G[6] ;
-    // Random graph
+    /*// Random graph
     for(int i = 0;i < 6;i++){
         Adj_G[i][i]=0;
         for(int j = i+1 ; j < 6 ; j++){
@@ -57,13 +54,13 @@ int main(){
         std::cout<<std::endl;
     }
 
-    
-    /*Adj_G[0]=19;
+    */
+    Adj_G[0]=19;
     Adj_G[1]=41;
     Adj_G[2]=20;
     Adj_G[3]=10;
     Adj_G[4]=37;
-    Adj_G[5]=50;*/
+    Adj_G[5]=50;
     
     /*for(int i = 0; i<6; i++){
         std::cout<<Adj_G[0][i];
@@ -78,9 +75,12 @@ int main(){
                            1, 1, 0, 0, 1, 0 };*/
 
     
-    coloring(Adj_G);
-
-
-
+    coloring(Adj_G,color_table,color_used);
+    std::cout<<"Color table ";
+    for(int i=0;i<6;i++){
+        std::cout<<color_table[i];
+    }
+    std::cout<<std::endl;
+    std::cout<<"Max number of colors "<<color_used<<std::endl;
     return 0;
 }
